4668f5e2651b1e671fc07a409902f803042163c2
Walter Schaertl <wps3581@g.rit.edu>
2017-10-01 11:56:38 -0400

    Adding a redirrect

--- NYRegentsPrep/urls.py
+++ NYRegentsPrep/urls.py
@@ -17,8 +17,10 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
 	url(r'^NYRP/', include('NYRP.urls')),
+	url(r'^', RedirectView.as_view(url='/NYRP/')),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
